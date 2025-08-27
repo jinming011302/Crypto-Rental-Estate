e7444ac14d0e1b9b9b2f970f560905862ae30eee
jinming011302 <jingming011302@gmail.com>
2025-08-27 12:45:41 +0800

    init

---
+++ frontend/src/app/signin.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function SignIn() {
+  return (
+    <div>
+      <Image src="/next.svg" alt="Next.js logo" width={180} height={38} />
+    </div>
+  );
+}
